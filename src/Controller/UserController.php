3999e39dc543e7ae5702a501218f5c21be521ed1
CaroCap <carolinecap.event@gmail.com>
2022-03-16 12:49:09 +0100

    Formulaire d'enregistrement User

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
@@ -15,4 +17,32 @@ class UserController extends AbstractController
             'controller_name' => 'UserController',
         ]);
     }
+
+    // #[Route('/user/create', name: 'afficher_form_user')]
+    // public function afficherFormUser(): Response
+    // {
+    //     // Créa du Form User
+    //     $user = new User();
+    //     $formulaireUser = $this->createForm(UserType::class, $user, ['method'=>'POST', 'action'=>$this->generateUrl('add_user')]);
+
+    //     // on envoie un objet FormView à la vue pour qu'elle puisse 
+    //     // faire le rendu, pas le formulaire en soi
+    //     $vars = ['unFormulaireUser' => $formulaireUser->createView()];
+
+    //     return $this->render('user/formUser.html.twig', $vars);
+    // }
+
+    // #[Route('/user/create', name: 'add_user')]
+    // public function traitementFormUser(): Response
+    // {
+    //     // // Créa du Form User
+    //     // $user = new User();
+    //     // $formulaireUser = $this->createForm(UserType::class, $user, ['method'=>'POST', 'action'=>$this->generateUrl('add_user')]);
+
+    //     // // on envoie un objet FormView à la vue pour qu'elle puisse 
+    //     // // faire le rendu, pas le formulaire en soi
+    //     // $vars = ['unFormulaireUser' => $formulaireUser->createView()];
+
+    //     return $this->render('user/traitementUser.html.twig');
+    // }
 }
---
+++ src/Form/CategorieType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+            // ->add('adresse', EntityType::class)
+            // ->add('categorie', EntityType::class)
+            ->add('telephone', TelType::class)
+            ->add('dateNaissance', BirthdayType::class)
+            ->add('photo', TextType::class)
+            ->add('accordPhoto', CheckboxType::class)
+            ->add('persContactNom', TextType::class)
+            ->add('persContactTel', TelType::class)
+            ->add('persContactMail', EmailType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
